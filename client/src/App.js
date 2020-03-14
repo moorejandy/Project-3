8901510c9afaabf6a31fb10b3282831e7a3464c2
Andy Moore <moorejandy@gmail.com>
2020-03-14 12:44:44 -0400

    working react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,24 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Nav";
-import UserForm from "./components/UserForm";
+import UserForm from "./pages/UserForm";
 
 
 class App extends Component {
   render() {
     return (
-      // <Router>
       <div>
-        <Nav />
-        <UserForm />
-        {/* <Switch>
-          <Route exact path="/form" component={Form} />
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books/:id" component={Detail} /> */}
-          {/* /* <Route component={NoMatch} />
-        </Switch> */}
-      </div>
-    // </Router>
-
+      <Nav />
+      <Router>
+        <Switch>
+          <Route exact path="/form" component={UserForm} />
+          {/* <Route exact path="/user" component={userPage} /> */}
+          {/* <Route exact path="/user/:id" component={Detail} />  */}
+          </Switch>
+    </Router>
+    </div>
     );
   }
 }
